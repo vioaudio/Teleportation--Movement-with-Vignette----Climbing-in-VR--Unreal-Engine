0aac56b90d0de526deeb016c9e309be31898db6d
James Powell <55001186+vioaudio@users.noreply.github.com>
2020-04-10 22:08:07 -0700

    Sync Up Camera & Root

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/ArchitectureExplorer/VRCharacter.generated.h
@@ -0,0 +1,96 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ARCHITECTUREEXPLORER_VRCharacter_generated_h
+#error "VRCharacter.generated.h already included, missing '#pragma once' in VRCharacter.h"
+#endif
+#define ARCHITECTUREEXPLORER_VRCharacter_generated_h
+
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_SPARSE_DATA
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_RPC_WRAPPERS
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVRCharacter(); \
+	friend struct Z_Construct_UClass_AVRCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AVRCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArchitectureExplorer"), NO_API) \
+	DECLARE_SERIALIZER(AVRCharacter)
+
+
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAVRCharacter(); \
+	friend struct Z_Construct_UClass_AVRCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AVRCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/ArchitectureExplorer"), NO_API) \
+	DECLARE_SERIALIZER(AVRCharacter)
+
+
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRCharacter(AVRCharacter&&); \
+	NO_API AVRCharacter(const AVRCharacter&); \
+public:
+
+
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRCharacter(AVRCharacter&&); \
+	NO_API AVRCharacter(const AVRCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AVRCharacter)
+
+
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__Camera() { return STRUCT_OFFSET(AVRCharacter, Camera); }
+
+
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_9_PROLOG
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_SPARSE_DATA \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_RPC_WRAPPERS \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_INCLASS \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_SPARSE_DATA \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ARCHITECTUREEXPLORER_API UClass* StaticClass<class AVRCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ArchitectureExplorer_Source_ArchitectureExplorer_VRCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/ArchitectureExplorer/VRCharacter.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+
+// Sets default values
+AVRCharacter::AVRCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VR Root"));
+	VRRoot->SetupAttachment(GetRootComponent());
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(VRRoot);
+
+}
+
+// Called when the game starts or when spawned
+void AVRCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVRCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//Calculate how much HMD moved in this frame
+	FVector NewCameraOffset = Camera->GetComponentLocation() - GetActorLocation();
+	
+	//Removes Up & Down (only works if Up is on the Z axis)
+	NewCameraOffset.Z = 0;
+
+	//Move Actor the same way camera moved
+	AddActorWorldOffset(NewCameraOffset);
+
+	//Move VR Root in opposite way
+	VRRoot->AddWorldOffset(-NewCameraOffset);
+
+}
+
+// Called to bind functionality to input
+void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AVRCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Right"), this, &AVRCharacter::MoveRight);
+}
+
+void AVRCharacter::MoveForward(float throttle)
+{
+	AddMovementInput(throttle * Camera->GetForwardVector());
+}
+
+void AVRCharacter::MoveRight(float throttle)
+{
+	AddMovementInput(throttle * Camera->GetRightVector());
+}
+
